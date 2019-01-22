8c4218d76b44e50a256a72ca3ba5c25d73280f8c
ChristopherPotvin <chrispotvin4@gmail.com>
2019-01-22 07:37:23 -0700

    Second C Sharp Lab
    Incorporating a class library with validation and unit tests.

---
+++ ChristopherPotvin_CPRG200_Lab2.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChristopherPotvin_CPRG200_Lab2.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChristopherPotvin_CPRG200_Lab2.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa7c7a91-e242-4ff4-bdd1-ee4bb520669d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
